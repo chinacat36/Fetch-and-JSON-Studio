35c9c2fce8f135ee0309297c20f766766a360201
Nicole Holder <nholder06@gmail.com>
2019-11-06 20:24:37 -0600

    done with studio, except for the bonus

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            let list = [];
+        //     let spaceHours = [];
+            for(let i = 0; i<json.length; i++){
+        // spaceHours.push(json[i].hoursInSpace);
+        // spaceHours.sort((a,b) => b-a);
+        // console.log(spaceHours);
+        let container = document.getElementById("container");
+        list.push(
+        `<div class = "astronaut">
+        <div class = "bio">
+        <h3> ${json[i].firstName} ${json[i].lastName} </h3>
+        <ul>
+        <li>Hours in Space: ${json[i].hoursInSpace}</li>
+        <li>Active: ${json[i].active}</li>
+        <li>Skills: ${json[i].skills}</li>
+        </ul>
+        </div>
+        <img class = "avatar" src = ${json[i].picture}>
+        </div>
+        `)}
+        container.innerHTML = list;
+    });
+    });
+    });    
